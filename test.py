6f699879b207dc79fb984b9799de0ce48e90ac10
LimaHenrique <henrique_margarido@hotmail.com>
2019-05-01 12:20:49 -0300

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from pages.pages.pageObject import Home
 class TestPageHome(TestCase):
 
     def setUp(self):
-        self.driver=webdriver.Chrome('driver/chromedriver.exe')
+        self.driver=webdriver.Chrome('/var/jenkins_home/workspace/DesafioDevOps/driver/chromedriver.exe')
         self.page_home=Home(self.driver)
         self.driver.get('https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
 
